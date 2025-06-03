952a454fc2405963cb34742660f2fd75067b4bb5
Sean Simmons <seanrobinsimmons@gmail.com>
2025-06-03 07:33:39 -0400

    Imported breadcrumb component from shadcn. added to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 
 import './App.css'
 import { GraphControls } from './components/GraphControls'
+import {  Breadcrumb,
+          BreadcrumbList,
+          BreadcrumbItem,
+          BreadcrumbLink,
+          BreadcrumbPage,
+          BreadcrumbSeparator,
+          BreadcrumbEllipsis } from './components/ui/breadcrumb'
 
 function App() {
 
   return (
     <div className="relative min-h-screen bg-gray-100">
       <GraphControls />
+      <Breadcrumb className="fixed top-4 left-4 z-50 rounded-xl overflow-hidden">
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="#">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="#">Graph</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
+            <BreadcrumbPage>Controls</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
     </div>
   )
 }

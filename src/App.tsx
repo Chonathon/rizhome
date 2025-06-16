b1465f121af88b239ff5b9b554d2bb760a538098
b-price <bprice12@students.towson.edu>
2025-06-16 12:25:26 -0400

    refactor bc header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import './App.css'
 import { useState } from 'react'
 import { GraphControls } from './components/GraphControls'
-import {  Breadcrumb,
-          BreadcrumbList,
-          BreadcrumbItem,
-          BreadcrumbLink,
-          BreadcrumbPage,
-          BreadcrumbSeparator,
-          BreadcrumbEllipsis } from './components/ui/breadcrumb'
 import { Waypoints } from 'lucide-react'
+import { BreadcrumbHeader } from './components/BreadcrumbHeader'
 import { Button, buttonVariants } from "@/components/ui/button"
 import useArtists from "@/hooks/useArtists";
 import useGenres from "@/hooks/useGenres";
@@ -29,47 +23,14 @@ function App() {
       {/* UI component for interacting with the graph*/}
       <GraphControls />
 
-      {/* Breadcrumb navigation updates dynamically based on selected genre and artist */}
-      <Breadcrumb className="fixed top-4 left-4 z-50 p-2 rounded-xl overflow-hidden">
-        <BreadcrumbList>
-
-          {/* Home icon - always visible */}
-          <BreadcrumbItem>
-            <BreadcrumbLink onClick={() => {
-              setSelectedGenre(undefined)
-              setSelectedArtist(undefined)
-            }}>{<Waypoints size={20} />}</BreadcrumbLink>
-          </BreadcrumbItem>
-
-          {/* Show selected genre if available */}
-          {selectedGenre && (
-            <>
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                {selectedArtist ? (
-                  // Clickable link that sets selectedArtist to null
-                  <BreadcrumbLink onClick={() => {
-                    setSelectedArtist(undefined)
-                    // TODO: Add logic to reset graph view to genre level
-                  }}>{selectedGenre}</BreadcrumbLink>
-                ) : (
-                  <BreadcrumbPage>{selectedGenre}</BreadcrumbPage>
-                )}
-              </BreadcrumbItem>
-            </>
-          )}
-
-          {/* Show selected artist if available */}
-          {selectedGenre && selectedArtist && (
-            <>
-              <BreadcrumbSeparator />
-              <BreadcrumbItem>
-                <BreadcrumbPage>{selectedArtist.name}</BreadcrumbPage>
-              </BreadcrumbItem>
-            </>
-          )}
-        </BreadcrumbList>
-      </Breadcrumb>
+        {/* Breadcrumb navigation */}
+        <BreadcrumbHeader
+            selectedGenre={selectedGenre}
+            setSelectedGenre={setSelectedGenre}
+            selectedArtist={selectedArtist}
+            setSelectedArtist={setSelectedArtist}
+            HomeIcon={Waypoints}
+        />
 
       {/* Genres Graph */}
         {!selectedArtist && !selectedGenre && (

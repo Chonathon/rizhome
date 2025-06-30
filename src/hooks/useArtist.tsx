c654eabc96689422b772550d06442e7fd3ce25d1
b-price <bprice12@students.towson.edu>
2025-06-30 11:00:46 -0400

    optional .env file to easily switch between a local and hosted server

--- src/hooks/useArtist.tsx
+++ src/hooks/useArtist.tsx
@@ -1,8 +1,11 @@
 import {useEffect, useState} from "react";
 import {LastFMArtistJSON} from "@/types";
 import axios, {AxiosError} from "axios";
+import {envBoolean} from "@/lib/utils";
 
-const url = `https://rhizome-server-production.up.railway.app/artists/data/`;
+const url = envBoolean(import.meta.env.VITE_USE_LOCAL_SERVER)
+    ? import.meta.env.VITE_LOCALHOST
+    : import.meta.env.VITE_SERVER_URL || `https://rhizome-server-production.up.railway.app`;
 
 const useArtist = (mbid?: string) => {
     const [artistData, setArtistData] = useState<LastFMArtistJSON>();
@@ -13,7 +16,7 @@ const useArtist = (mbid?: string) => {
         if (mbid) {
             setArtistLoading(true);
             try {
-                const response = await axios.get(`${url}${mbid}`);
+                const response = await axios.get(`${url}/artists/data/${mbid}`);
                 setArtistData(response.data);
                 setArtistError(undefined);
             } catch (err) {
--- src/hooks/useGenreArtists.tsx
+++ src/hooks/useGenreArtists.tsx
@@ -1,8 +1,11 @@
 import {useEffect, useState} from "react";
 import {Artist, NodeLink} from "@/types";
 import axios, {AxiosError} from "axios";
+import {envBoolean} from "@/lib/utils";
 
-const url = `https://rhizome-server-production.up.railway.app/artists/`;
+const url = envBoolean(import.meta.env.VITE_USE_LOCAL_SERVER)
+    ? import.meta.env.VITE_LOCALHOST
+    : import.meta.env.VITE_SERVER_URL || `https://rhizome-server-production.up.railway.app`;
 
 const useGenreArtists = (genre?: string) => {
     const [artists, setArtists] = useState<Artist[]>([]);
@@ -14,7 +17,7 @@ const useGenreArtists = (genre?: string) => {
         if (genre) {
             setArtistsLoading(true);
             try {
-                const response = await axios.get(`${url}"${genre}"`);
+                const response = await axios.get(`${url}/artists/"${genre}"`);
                 setArtists(response.data.artists);
                 setArtistLinks(response.data.links);
             } catch (err) {

22d2031293b8305d50829851b528c9623f771479
Sean Simmons <seanrobinsimmons@gmail.com>
2025-07-11 12:40:20 -0400

    purge tailwind, removed use client from theme provider

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  purge: [],
   darkMode: "class",
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },

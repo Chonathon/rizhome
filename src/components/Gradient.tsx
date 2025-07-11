f6994b51c5187af5fd67cdd601f66c231531f76f
Sean Simmons <seanrobinsimmons@gmail.com>
2025-07-11 08:51:01 -0400

    noise effect for dark mode

--- src/components/Gradient.tsx
+++ src/components/Gradient.tsx
@@ -23,6 +23,7 @@ export function Gradient() {
                 <div className="-z-2 fixed top-1/2 left-1/2 w-screen aspect-square h-full -translate-x-1/2 -translate-y-1/2 pointer-events-none backdrop-blur-xs [mask-image:radial-gradient(circle_at_center,rgba(255,255,255,0)_60%,rgba(255,255,255,0.2)_70%,rgba(255,255,255,.8)_97%)]"/>
                 <div className="fixed top-1/2 left-1/2 w-screen aspect-square h-full -translate-x-1/2 -translate-y-1/2 pointer-events-none z-10
                 bg-[radial-gradient(circle_at_center,rgba(0,0,0,0)_60%,rgba(0,0,0,0.2)_70%,rgba(0,0,0,.8)_97%)]" />
+                <div className="fixed inset-0 pointer-events-none noise-dark" /> 
             </> }
         </>
     );
